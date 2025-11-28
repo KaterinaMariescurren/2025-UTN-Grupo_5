6f07f8f140dfbb86aa0381e71b4c49d53d98d4c6
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2025-11-27 23:21:53 -0300

    feat: se agregaron tests para el CI

---
+++ be_your_eyes_backend/app/tests/test_auth.py
@@ -0,0 +1,80 @@
+def test_register_persona(client):
+    payload = {
+        "email": "test@local.com",
+        "contrasenia": "1234",
+        "tipo": "persona",
+        "persona": {
+            "nombre": "nombre persona",
+            "tipo": "persona"
+        }
+    }
+
+    response = client.post("/register", json=payload)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+
+
+def test_login(client):
+    client.post("/register", json={
+        "email": "login@test.com",
+        "contrasenia": "abcd",
+        "tipo": "persona",
+        "persona": {
+            "nombre": "Pepe Argento",
+            "tipo": "persona"
+        }
+    })
+
+    response = client.post("/login", json={
+        "email": "login@test.com",
+        "contrasenia": "abcd"
+    })
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "access_token" in data
+    assert data["tipo"] == "persona"
+
+
+def test_obtener_local_id(client):
+    l = client.post("tipos_local/", json={"nombre": "Restaurante"})
+    id_tipo = l.json()["id"]
+    r = client.post("/register", json={
+    "email": "local@test.com",
+    "contrasenia": "123",
+    "tipo": "local",
+    "local": {
+        "nombre": "Local Test",
+        "telefono": "2284223567",
+        "tipo_local_id": id_tipo,
+        "direccion": {
+            "calle": "Calle Falsa",
+            "altura": "123",
+            "codigo_postal": "7400"     
+        },
+        "horarios": [
+            {
+                "dia": "lunes",
+                "horario_apertura": "09:00",
+                "horario_cierre": "18:00"
+            }
+        ]
+    }
+})
+  
+    print(r.status_code, r.json())
+
+    token = r.json()["access_token"]
+
+    response = client.get(
+        "/me/local_id",
+        headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == 200
+    assert "local_id" in response.json()
---
+++ be_your_eyes_backend/app/tests/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+from app.main import app
+
+@pytest.mark.asyncio
+async def test_main():
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "BeYourEyes API activa"}
