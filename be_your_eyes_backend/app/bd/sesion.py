2427f88e6d06af98a3b1e1133f1255618e877183
Geronimo Makowiecki <gmakowiecki@alu.frlp.utn.edu.ar>
2025-11-28 01:19:58 +0000

    feat(DevOps): Dockerfile + CI/CD con Render
    Cambie requirimientos tambien

--- be_your_eyes_backend/app/bd/sesion.py
+++ be_your_eyes_backend/app/bd/sesion.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./be_your_eyes.bd"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@localhost:5432/be_your_eyes_db"  
 )
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
